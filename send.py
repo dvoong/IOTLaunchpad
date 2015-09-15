37dfdf878c2b9bde41df00fbde8463a00db33412
David Voong <voong.david@gmail.com>
2015-09-15 19:58:03 +0100

    Add basic functionality for mongodb

---
+++ send.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+import socket
+import logging
+import argparse
+import json
+
+logger = logging.getLogger(__name__)
+
+class Socket(socket.socket):
+
+    def __init__(self, x=socket.AF_INET, y=socket.SOCK_DGRAM, *args, **kwargs):
+        super(Socket, self).__init__(x, y, *args, **kwargs)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Sends a UDP packet to a socket.")
+    parser.add_argument('--msg', default='Hello world!')
+    parser.add_argument('--ip', default='127.0.0.1')
+    parser.add_argument('--port', type=int, default=9999)
+    args = parser.parse_args()
+
+    logging.basicConfig(level=logging.INFO)
+
+    logger.info('args: {}'.format(args))
+
+    sock = Socket() #socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    logger.info('Sending message to {ip}:{port}: msg: "{message}"\n'.format(ip=args.ip, port=args.port, message=args.msg))
+    
+    response = sock.sendto(args.msg, (args.ip, args.port))
+    
+    logger.info('Response: {}'.format(response))
+
+    msg = {
+        'x': 10,
+        'y': 11,
+        'z': 12,
+    }
+    response = sock.sendto(json.dumps(msg), (args.ip, args.port))
---
+++ start_server.py
@@ -0,0 +1,39 @@
+import json
+import logging
+from twisted.internet.protocol import DatagramProtocol
+from twisted.internet import reactor
+from pymongo import MongoClient
+
+logger = logging.getLogger(__name__)
+
+class Echo(DatagramProtocol):
+
+    def datagramReceived(self, data, (host, port)):
+        try:
+            json_ = json.loads(data)
+            client = MongoClient()
+            logging.info('MongoDB client: {}'.format(client))
+            logging.info('Creating/getting collection: tfl_data')
+            db = client.tfl_data
+            logging.info('db: {}'.format(db))
+            logging.info('Creating/getting my_collection')
+            collection = db.my_collection
+            logging.info('my_collection: {}'.format(collection))
+            logging.info('inserting entry into collection:')
+            result = collection.insert_one({'test': 0})
+            logging.info('returned output: {}'.format(result))
+            logging.info('collection contents:')
+            cursor = collection.find()
+            for document in cursor:
+                logging.info(document)
+            
+        except ValueError as e:
+            print "received %r from %s:%d" % (data, host, port)
+        
+        # self.transport.write(data, (host, port))
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    
+    reactor.listenUDP(9999, Echo())
+    reactor.run()
